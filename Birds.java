fd925cf14f811605ae5ca86e57861a7cdfbc6bd3
Vladimir1986 <SokolovVladimir1986@mail.ru>
2023-08-02 12:13:23 +0700

    added class Birds

---
+++ Birds.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Birds {
+    String Kind;
+    int Age;
+    double Weight;
+
+    Birds(){}
+    Birds(String Kind_){
+        Kind = Kind_;
+    }
+
+    public void Chiric(){
+        System.out.println("Chiric-chiric");
+    }
+}
--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Cats c1 = new Cats("Ruskiy");
         c1.Miau();
+
+        Birds b1 = new Birds();
+        b1.Chiric();
     }
 }
