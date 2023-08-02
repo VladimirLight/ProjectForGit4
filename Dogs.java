f1f273f26daddf8a633c5a70e35cfc00506cd006
Vladimir1986 <SokolovVladimir1986@mail.ru>
2023-08-02 11:52:06 +0700

    added class Dogs

---
+++ Dogs.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Dogs extends Cats{
+
+    Dogs(){}
+
+    Dogs(String Breed_){
+        Breed = Breed_;
+    }
+
+    public void Gav(){
+        System.out.println("Gav-gav!");
+    }
+}
--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Cats c1 = new Cats("Ruskiy");
         c1.Miau();
+
+        Dogs d1 = new Dogs("Taksa");
+        d1.Gav();
     }
 }
