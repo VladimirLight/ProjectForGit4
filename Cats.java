8fd9fe73ba08f90384d1c87ec9fa7d09b571680a
Vladimir1986 <SokolovVladimir1986@mail.ru>
2023-08-02 11:42:29 +0700

    added class Cats

---
+++ Cats.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Cats {
+
+    String Breed;
+    int Age;
+    double Weight;
+
+    Cats(){}
+
+    Cats(String Breed_){
+        Breed = Breed_;
+    }
+
+    public void Miau(){
+        System.out.println("Miiaauu");
+    }
+}
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cats c1 = new Cats("Ruskiy");
+        c1.Miau();
     }
 }
